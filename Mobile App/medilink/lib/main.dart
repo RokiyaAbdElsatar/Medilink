9e28f4642bece060a07986b7a8c0f2da2612bddb
Rokiya2932004 <rokiyaabdelstar293@gmail.com>
2025-09-12 17:01:24 +0300

    first commit

---
+++ Mobile App/medilink/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:medilink/views/Doctor%20Mode/onboarding1.dart';
+
+void main() {
+  runApp(const MedilinkApp());
+}
+
+class MedilinkApp extends StatelessWidget {
+  const MedilinkApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Medilink',
+
+      home: const Onboarding1(),
+    );
+  }
+}
